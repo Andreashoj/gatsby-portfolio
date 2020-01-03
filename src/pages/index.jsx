4a994d256ea2ddaed6e9c1fca0e4217540c0e20d
Andreas <andrewhoj@gmail.com>
2020-01-03 16:06:07 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,10 +3,10 @@ import Layout from "../layout"
 import "../layout/index.css"
 import { ContextWrapper } from "../contexts/ContextWrapper"
 
-const index = () => {
+const Index = () => {
   const { count } = useContext(ContextWrapper)
   console.log(count)
   return <Layout>asd</Layout>
 }
 
-export default index
+export default Index
